0ab1b6d9ecc4d26d5a90ecdabd108bc9bc79f595
mungmoong1004 <bruce8110500@gmail.com>
2025-05-25 00:20:57 +0900

    애플

--- upload_test.py
+++ upload_test.py
@@ -1,4 +1,18 @@
-print("유튜브 업로드 테트스")
+print("유튜브 업로드 테트스 입니다") 
+print("hello") 
+print("유튜브 업로드 테트스 입니다") 
+print("l love you always")
+print("hello world you are my world")
+print("성훈 love you always")
+print("오늘은 2025년 5월 25일 입니다.")
 print("hello world")
+print("성훈 love you always")
+print("오늘은 2025년 5월 25일 입니다.")
+print("hello world")
+print("성훈 love you always")
+print("오늘은 2025년 5월 25일 입니다.")
+print("hello world")
+print("apple")
+
 
 
